cf8288ba2a9d4ef60087e8427a0dd2c5d474e667
Kaosevil <dedrisproject@gmail.com>
2022-05-11 18:39:57 +0200

    fix

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-//ini_set('display_errors', '1');
-//ini_set('display_startup_errors', '1');
-//error_reporting(E_ALL);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 if (empty($_POST)) {
 ?>
 <html>
@@ -820,7 +820,7 @@ echo "?&gt;
 </code></pre>";	
 echo "</div>";	
 //}
-}
+
 	
 
 
